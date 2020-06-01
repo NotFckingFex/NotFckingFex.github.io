c56a1617afe2f7c1c4fcbd20984c035b93e55dfb
Felix B <62483138+NotFckingFex@users.noreply.github.com>
2020-06-01 21:34:07 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/* This code is written by: Felix Burwitz.
+For licencing, Copyright issues etc. contact me at:
+reinermagdenernst@gmail.com
+this is NOT my business E-Mail - after you contact me i will give it to you.
+Web: https://www.fex.codes
+Discord:  NotFckingFex#6636
+*/
+?>
+
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Polizeicomputer</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+</head>
+<body>
+<!-- CSS for the look -->
+<div id="topright">
+    POLICE COMPUTER V.0.1
+</div>
+<div id="colorstrip"></div>
+<img src="img/logo.png" id="logo" alt="Police Logo"/>
+<!-- END -->
+<div class="footer">
+    <p>This is a monitored Federal System</p>
+    <p>for authorized users only</p></p>
+    <p>Logged in as: OFFICERNAME</p>
+</div>
+
+</body>
+</html>
